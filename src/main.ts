ac28eaab9bf7bc94bbb0068e9a4712fdc06e90d0
cclintris <181250083@smail.nju.edu.cn>
2021-12-15 14:40:02 +0800

    add: update ts configuration

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import './index.less'
 
-const tick = () => {
-  let $time = document.getElementById('js-time')
+const tick = (): void => {
+  let $time: HTMLElement = document.getElementById('js-time')
   $time.textContent = new Date().toLocaleString()
   setTimeout(tick, 1000)
 }
