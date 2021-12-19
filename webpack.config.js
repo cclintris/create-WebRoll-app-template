32a37442516ca9f71812b6ba04761ab31600a4ff
cclintris <181250083@smail.nju.edu.cn>
2021-12-19 11:59:54 +0800

    add: update webpack configuration based on production/devemopment env

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path')
+const webpack = require('webpack')
 const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const isProd = process.env.npm_lifecycle_event === 'build'
 
 const config = {
@@ -16,7 +18,7 @@ const config = {
         test: /\.less/,
         use: [
           // 'style-loader' alternative
-          // need to use it with HtmlWebpackPlugin
+          // need to use it with html-webpack-plugin
           {
             loader: MiniCSSExtractPlugin.loader,
           },
@@ -50,10 +52,42 @@ const config = {
     new HtmlWebpackPlugin({
       template: path.join(__dirname, 'public/index.html'),
     }),
+  ],
+}
+
+if (!isProd) {
+  // development mode
+  config.entry.push('webpack-dev-server/client?http://localhost:8085/')
+  config.devtool = 'inline-source-map'
+  config.plugins.push(
     new MiniCSSExtractPlugin({
       filename: '[name].css',
-    }),
-  ],
+      chunkFilename: '[id].css',
+    })
+  )
+  config.devServer = {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    historyApiFallback: true,
+    compress: true,
+    port: 8085,
+    hot: true,
+    client: {
+      overlay: true,
+      progress: true,
+    },
+    open: true,
+  }
+} else {
+  // production mode
+  config.plugins.push(
+    new CleanWebpackPlugin(),
+    new MiniCSSExtractPlugin({
+      filename: '[name].[contenthash].css',
+      chunkFilename: '[id].[contenthash].css',
+    })
+  )
 }
 
 module.exports = config
